dfeeaba03c4851aeb197cfef26d22344a77afe9a
ShirJu10 <sjusid@frba.utn.edu.ar>
2025-04-17 15:33:36 -0300

    probabilidadesLluvias terminado

--- proyecto-test/src/Library.hs
+++ proyecto-test/src/Library.hs
@@ -3,6 +3,13 @@ import PdePreludat
 
 signo :: Number -> Char
 signo numero  | numero > 0 = '+'
-              | numero < 0 = '-'
+              | numero < 0 = '-'--ingresar numero negativo entre () 
               | numero == 0 = '0'
 
+
+
+probLluvias :: String -> Number --ingresar string entre "" 
+probLluvias clima  | clima == "despejado" = 0 
+                   | clima == "nublado" = 25 
+                   | clima == "lluvioso" = 100 
+                   | otherwise = 50 
