56c746b5042bd26d374b8dc329d9355bef208094
ShirJu10 <sjusid@frba.utn.edu.ar>
2025-04-18 17:28:50 -0300

    geometria

--- proyecto-test/src/Library.hs
+++ proyecto-test/src/Library.hs
@@ -22,6 +22,8 @@ bisiesto anio  | mod anio 4 == 0 && mod anio 100 /= 0 = True
                | mod anio 400 == 0 = True
                | otherwise = False
 
+-- OPERACIONES
+
 esMultiploDeTres :: Number -> Bool
 esMultiploDeTres numero = mod numero 3 == 0
 
@@ -30,3 +32,20 @@ esMultiploDe numero multiplo = mod multiplo numero == 0
 
 esPar :: Number -> Bool
 esPar numero = even numero
+
+--GEOMETRIA
+
+areaRectangulo :: Number -> Number -> Number
+areaRectangulo base altura = base * altura
+
+volumenCubo :: Number -> Number
+volumenCubo lado = lado ^ 3
+
+superficieCeleste :: Number -> Number
+superficieCeleste lado = lado * lado
+
+superficieAzulClaro :: Number -> Number
+superficieAzulClaro lado = pi * (lado / 2) ^ 2
+
+superficieAzulOscuro:: Number -> Number
+superficieAzulOscuro lado = lado ^ 2 / 2
