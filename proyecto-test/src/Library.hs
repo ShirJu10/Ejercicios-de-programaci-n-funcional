b877caa86507c085544924e5f3adbf8299bda67d
ShirJusid <sjusid@frba.utn.edu.ar>
2025-04-25 11:49:08 -0300

    Update Library.hs
    ejercicio nomus y clase 25/4

--- proyecto-test/src/Library.hs
+++ proyecto-test/src/Library.hs
@@ -79,3 +79,78 @@ potencia base exponente
 --Averiguar si un número es cuadrado perfecto
 cuadradoPerfecto :: Number -> Bool 
 cuadradoPerfecto num = (sqrt num) ^ 2 == num -- no se porque no me da con 9 
+
+--CLASE 25/4
+
+data Figura = Rectangulo {base :: Number , altura :: Number}
+            | Triangulo {lado :: Number}
+            | Circulo {radio :: Number}
+            deriving Show
+
+-- funcion definida por partes (una parte para el rectangulo y otra para el perimetro)
+perimetro :: Figura -> Number
+--con patern matching estoy desarmando el constructor "Rectangulo"
+--Para ingresar valores definir r1 = Rectangulo num num
+perimetro (Rectangulo base altura) = (base + altura) * 2
+perimetro (Triangulo lado) = 3 * lado
+perimetro (Circulo radio) = 2 * pi * radio
+
+--hacer funcion superficie y "esRegular"
+--superficie :: Figura -> Number
+--superficie (rectangulo)
+
+--esRegular es una funcion que confice el filter
+esRegular (Rectangulo base altura) = base == altura
+esRegular (Triangulo lado) = True
+esRegular (Circulo radio) = True
+
+l1 = [Rectangulo 2 2, Circulo 4, Triangulo 5, Rectangulo 3 6]
+
+--data Palabra = Letras {tamanio :: Number}
+            --deriving Show
+
+esCorta :: String -> Bool
+esCorta palabra = length palabra < 5
+
+s1 = ["hola", "shirly", "juju", "jujuuuuuuuu"] --para ejecutar: filter esCorta s1
+
+--PARTE 1 Los Nomus son humanos mutados que poseen distintas capacidades físicas, como tener alas, múltiples brazos, cantidad de ojos y el color de piel, además de tener una cantidad de vida y fuerza.
+
+--Luego se nos pide averiguar si puede ver, es decir, si tiene ojos y su categoría.
+
+--Categorías de un Nomu:
+
+-- Los nomus pichis son aquellos que su fuerza es inferior a 100
+
+-- Los nomus comunes son aquellos que su fuerza es inferior a 3000 y mayor a 1000
+
+-- Los nomus fuertes son aquellos que su fuerza es inferior a 10000 y mayor que 3000
+
+-- Por último, los nomus high-end poseen una fuerza mayor a 10000
+
+
+--PARTE 2 Dada una lista de nomus se quiere entrenarlos y saber si el ejercito puede ir a la guerra despues del entrenamiento. 
+--Entrenar un nomu aumenta su fuerza dos mil veces y para que un ejercito pueda ir a la guerra todos los nomus deben tener una fuerza mayor a 2500. Ademas queremos saber cuales nomus de nuestro ejercito, para eso deben cumplir la condicion de la parte 1.
+
+data Nomus = Nomu {ojos:: Number, fuerza :: Number}
+            deriving Show
+
+categoria :: Nomus -> String
+categoria (Nomu _ fuerza) 
+            | fuerza < 1000 = "Nomus Pichis"
+            | fuerza < 3000 && fuerza > 1000 = "Nomus Comunes"
+            | fuerza < 10000 && fuerza > 3000 = "Nomus Fuertes"
+            | otherwise = "Nomus High-end"
+
+vision :: Nomus -> Bool
+vision nomu = ojos nomu > 0 
+
+nomu1 :: Nomus 
+nomu1 = Nomu {ojos = 0, fuerza = 123} 
+nomu2 :: Nomus 
+nomu2 = Nomu {ojos = 5, fuerza = 9748} 
+
+--parte 2
+nomus = [nomu1, nomu2]
+ejercito :: [Nomu]
+ejercito nomus = 
